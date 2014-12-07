a34216c08ab624b0c271176c5832bf78ae8b5616
MotherBrain <aespielberg@ubuntu.(none)>
2014-12-06 19:55:09 -0800

    Added ability to read labelme data from web and store it to local images

---
+++ client/src/db_proj/Experimental.java
@@ -0,0 +1,62 @@
+package db_proj;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Experimental {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		String csvFile = "../data/manifest.txt";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = "\t";
+		String prefix = "http://labelme.csail.mit.edu/Images";
+
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] url = line.split(cvsSplitBy);
+				if (url[1].endsWith(".jpg")){
+					String whole_url = prefix + url[1].substring(1);
+					System.out.println(whole_url);
+					BufferedImage image = ImageUtils.loadWebImage(whole_url);
+					//ImageUtils.showImage(image);
+					//return;
+				}
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		System.out.println("Done");
+	}
+
+}
+
+
